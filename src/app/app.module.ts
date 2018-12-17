30b4181373dfcce0dd8e76f5d6f03aa3d1ae8ffc
klimentru1986 <kliment1986@inbox.ru>
2018-12-17 20:02:40 +0300

    hello component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ForModule } from './for/for.module';
 import { CounterModule } from './counter/counter.module';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   imports: [CounterModule, ForModule],
-  declarations: [AppComponent]
+  declarations: [AppComponent, HelloComponent]
 })
 export class AppModule {}
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css']
+})
+export class HelloComponent {
+  @Input()
+  title: string;
+}
