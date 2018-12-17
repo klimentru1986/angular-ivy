c0302f64b2ee316c1d1a11a000edd9a933d27e32
klimentru1986 <kliment1986@inbox.ru>
2018-12-17 19:58:22 +0300

    counter unsubscribe

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit, ɵmarkDirty as markDirty } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ɵmarkDirty as markDirty,
+  OnDestroy
+} from '@angular/core';
 import { CounterService } from './counter.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent implements OnInit, OnDestroy {
   counter = 0;
+  subscription = new Subscription();
 
   constructor(private counterService: CounterService) {}
 
   ngOnInit() {
-    this.counterService.getCounter().subscribe(c => {
-      this.counter = c;
-      markDirty(this);
-    });
+    this.subscription.add(
+      this.counterService.getCounter().subscribe(c => {
+        this.counter = c;
+        markDirty(this);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   increment() {
